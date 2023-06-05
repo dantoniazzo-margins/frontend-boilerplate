75dc94f3c4dfdcf7fc067d12673a55d501eb082c
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2023-06-05 12:59:46 +0200

    Fix: title and margins logo favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { lazy } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { Counter } from '@/features/counter/ui';
-const Home = lazy(() => import('./home'));
 
+const Home = lazy(() => import('./home'));
+const CounterPage = lazy(() => import('./counter/ui'));
 export const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} index />
-      <Route path="/counter" element={<Counter />} />
+      <Route path="/counter" element={<CounterPage />} />
     </Routes>
   );
 };
